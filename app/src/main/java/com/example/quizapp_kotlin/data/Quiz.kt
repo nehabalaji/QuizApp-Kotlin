4ebb0406592c35ce37e7f6426e7b45634393d117
nehabalaji <nehabalaji10@gmail.com>
2020-10-05 22:01:45 +0530

    add entity class

--- app/src/main/java/com/example/quizapp_kotlin/data/Quiz.kt
+++ app/src/main/java/com/example/quizapp_kotlin/data/Quiz.kt
@@ -1,4 +1,19 @@
 package com.example.quizapp_kotlin.data
 
-class Quiz {
-}
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Quiz(
+    @PrimaryKey
+    @ColumnInfo(name = "ID")
+    var id: Long,
+
+    @ColumnInfo(name = "StateName")
+    var stateName: String,
+
+    @ColumnInfo(name = "CapitalName")
+    var capitalName: String
+): Parcelable
