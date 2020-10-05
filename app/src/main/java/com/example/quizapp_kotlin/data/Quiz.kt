2cb8a4ff4514a39b9b4f12ef86e5aa2fcd89cebf
nehabalaji <nehabalaji10@gmail.com>
2020-10-05 22:14:18 +0530

    add dao

--- app/src/main/java/com/example/quizapp_kotlin/data/Quiz.kt
+++ app/src/main/java/com/example/quizapp_kotlin/data/Quiz.kt
@@ -2,10 +2,12 @@ package com.example.quizapp_kotlin.data
 
 import android.os.Parcelable
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity(tableName = "StateAndCapital")
 data class Quiz(
     @PrimaryKey
     @ColumnInfo(name = "ID")
--- app/src/main/java/com/example/quizapp_kotlin/database/QuizDao.kt
+++ app/src/main/java/com/example/quizapp_kotlin/database/QuizDao.kt
@@ -1,4 +1,20 @@
 package com.example.quizapp_kotlin.database
 
-class QuizDao {
+import androidx.paging.DataSource
+import androidx.room.*
+import com.example.quizapp_kotlin.data.Quiz
+
+@Dao
+interface QuizDao {
+    @Query("SELECT * FROM StateAndCapital")
+    fun getAllStates(): DataSource.Factory<Int, Quiz>
+
+    @Insert
+    fun insertState(quiz: Quiz)
+
+    @Update
+    fun updateState(quiz: Quiz)
+
+    @Delete
+    fun deleteState(quiz: Quiz)
 }
