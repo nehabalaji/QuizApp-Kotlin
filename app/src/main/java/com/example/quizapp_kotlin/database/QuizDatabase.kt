8e874149ba0de5c840b04b97d83e64b1b1bc86a7
nehabalaji <nehabalaji10@gmail.com>
2020-10-05 22:21:17 +0530

    add database

--- app/src/main/java/com/example/quizapp_kotlin/database/QuizDatabase.kt
+++ app/src/main/java/com/example/quizapp_kotlin/database/QuizDatabase.kt
@@ -1,4 +1,33 @@
 package com.example.quizapp_kotlin.database
 
-class QuizDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.quizapp_kotlin.data.Quiz
+import java.util.concurrent.Executors
+
+@Database(entities = [Quiz::class], version = 1, exportSchema = false)
+abstract class QuizDatabase: RoomDatabase() {
+
+    abstract val dao: QuizDao
+
+    companion object{
+        val executorService = Executors.newSingleThreadExecutor()
+
+        @Volatile
+        private var INSTANCE: QuizDatabase? = null
+
+        fun getInstance(context: Context): QuizDatabase{
+            synchronized(this){
+                var instance: QuizDatabase? = INSTANCE
+                if (instance==null){
+                    instance = Room.databaseBuilder(context.applicationContext, QuizDatabase::class.java, "QuizDatabase")
+                        .fallbackToDestructiveMigration().build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
