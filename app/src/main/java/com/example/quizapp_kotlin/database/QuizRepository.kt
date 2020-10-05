8148eaf64adc56d5ccd25610b8223cf5e9b34304
nehabalaji <nehabalaji10@gmail.com>
2020-10-05 22:29:36 +0530

    add repository

--- app/src/main/java/com/example/quizapp_kotlin/database/QuizRepository.kt
+++ app/src/main/java/com/example/quizapp_kotlin/database/QuizRepository.kt
@@ -1,4 +1,54 @@
 package com.example.quizapp_kotlin.database
 
-class QuizRepository {
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.example.quizapp_kotlin.data.Quiz
+import java.util.concurrent.Executors
+
+class QuizRepository (application: Application){
+    private val quizDao: QuizDao
+    private val executorService = Executors.newSingleThreadExecutor()
+
+    init {
+        val quizDatabase = QuizDatabase.getInstance(application)
+        quizDao = quizDatabase.dao
+    }
+
+    companion object{
+        private var quizRepository: QuizRepository? = null
+        fun getRepository(application: Application): QuizRepository? {
+            if (quizRepository==null){
+                synchronized(QuizRepository::class.java){
+                    if (quizRepository==null){
+                        quizRepository = QuizRepository(application)
+                    }
+                }
+            }
+            return quizRepository
+        }
+    }
+
+    fun insertState(quiz: Quiz){
+        executorService.execute {
+            quizDao.insertState(quiz)
+        }
+    }
+
+    fun updateState(quiz: Quiz){
+        executorService.execute {
+            quizDao.updateState(quiz)
+        }
+    }
+
+    fun deleteState(quiz: Quiz){
+        executorService.execute {
+            quizDao.deleteState(quiz)
+        }
+    }
+
+    fun getAllStates(): LiveData<PagedList<Quiz>>{
+        return LivePagedListBuilder(quizDao.getAllStates(), 10).build()
+    }
 }
