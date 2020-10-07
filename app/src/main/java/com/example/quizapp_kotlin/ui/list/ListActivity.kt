b2d8b7f3379b1011506dfa0a9c9be450a56f4f5c
nehabalaji <nehabalaji10@gmail.com>
2020-10-07 19:32:34 +0530

    make changes to list activity

--- app/src/main/java/com/example/quizapp_kotlin/ui/list/ListActivity.kt
+++ app/src/main/java/com/example/quizapp_kotlin/ui/list/ListActivity.kt
@@ -3,14 +3,21 @@ package com.example.quizapp_kotlin.ui.list
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.quizapp_kotlin.R
 import com.example.quizapp_kotlin.data.Quiz
+import com.example.quizapp_kotlin.database.QuizRepository
 import com.example.quizapp_kotlin.ui.add.AddActivity
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_list.*
 
-class ListActivity : AppCompatActivity() {
+class ListActivity(dragDirs: Int) : AppCompatActivity() {
 
     val EXTRA_STATE_NAME = "extra_state_name_to_be_updated"
     val EXTRA_CAPITAL_NAME = "extra_capital_name_to_be_updated"
@@ -34,6 +41,58 @@ class ListActivity : AppCompatActivity() {
         val recyclerView = recyclerView2
 
         recyclerView.layoutManager = LinearLayoutManager(this)
+        val listPagingAdapter = ListPagingAdapter()
+        recyclerView.adapter = listPagingAdapter
 
+        listViewModel.statesList.observe(this, Observer {
+            listPagingAdapter.submitList(it)
+        })
+
+        val clickListener: ListPagingAdapter.ClickListener =
+            object : ListPagingAdapter.ClickListener {
+                override fun onItemClick(position: Int, view: View) {
+                    val currentState: Quiz? = listPagingAdapter.getStateAtPosition(position)
+                    if (currentState != null) {
+                        launchUpdateActivity(currentState)
+                    }
+                }
+            }
+
+        val constraintLayout = constraintLayout1
+        val snackbar =
+            Snackbar.make(constraintLayout, "State Deleted", BaseTransientBottomBar.LENGTH_LONG)
+                .setAction("UNDO") {
+                    listViewModel.insertState(quiz)
+                }
+
+        val itemTouchHelper = ItemTouchHelper(object :
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position: Int = viewHolder.adapterPosition
+                quiz = listPagingAdapter.getStateAtPosition(position)!!
+                listViewModel.deleteState(quiz)
+                snackbar.show()
+            }
+
+        })
+
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+        listPagingAdapter.setItemClickListener(clickListener)
+    }
+
+    fun launchUpdateActivity(currentState: Quiz){
+        val intent = Intent(this, AddActivity::class.java)
+        intent.putExtra(EXTRA_STATE_ID, currentState.id)
+        intent.putExtra(EXTRA_STATE_NAME, currentState.stateName)
+        intent.putExtra(EXTRA_CAPITAL_NAME, currentState.capitalName)
+        startActivityForResult(intent, UPDATE_STATE_REQUEST_CODE)
     }
 }
