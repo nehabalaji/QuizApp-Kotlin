4250b147a8f8422556ceed7287fd105c6b7fb9d4
nehabalaji <nehabalaji10@gmail.com>
2020-10-09 22:48:10 +0530

    add addViewModel

--- app/src/main/java/com/example/quizapp_kotlin/ui/add/AddActivity.kt
+++ app/src/main/java/com/example/quizapp_kotlin/ui/add/AddActivity.kt
@@ -5,6 +5,16 @@ import android.os.Bundle
 import com.example.quizapp_kotlin.R
 
 class AddActivity : AppCompatActivity() {
+
+    val EXTRA_STATE_NAME = "extra_state_name_to_be_updated"
+    val EXTRA_CAPITAL_NAME = "extra_capital_name_to_be_updated"
+    val EXTRA_STATE_ID = "extra_state_id_to_be_updated"
+    val UPDATE_STATE_REQUEST_CODE = 1
+    val NEW_STATE_REQUEST_CODE = 2
+
+    private lateinit var stateName: String
+    private lateinit var capitalName: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
