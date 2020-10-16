eb36641adadb1392d9a756366dcf1b07d5a84004
nehabalaji <nehabalaji10@gmail.com>
2020-10-16 14:15:09 +0530

    make changes to dao interface

--- app/src/main/java/com/example/quizapp_kotlin/database/QuizDao.kt
+++ app/src/main/java/com/example/quizapp_kotlin/database/QuizDao.kt
@@ -2,6 +2,7 @@ package com.example.quizapp_kotlin.database
 
 import androidx.paging.DataSource
 import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteQuery
 import com.example.quizapp_kotlin.data.Quiz
 
 @Dao
@@ -17,4 +18,13 @@ interface QuizDao {
 
     @Delete
     fun deleteState(quiz: Quiz)
+
+    @RawQuery(observedEntities = [Quiz::class])
+    fun getAllStates(supportSQLiteQuery: SupportSQLiteQuery): DataSource.Factory<Int, Quiz>
+
+    @Query("SELECT * FROM StateAndCapital ORDER BY RANDOM() LIMIT 4")
+    fun getQuizState(): List<Quiz>
+
+    @Query("SELECT * FROM StateAndCapital ORDER BY RANDOM() LIMIT 1")
+    fun getRandomState(): Quiz
 }
