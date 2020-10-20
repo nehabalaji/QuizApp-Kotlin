74c6f9ba6889e126317db2870a0d08552700eeaf
nehabalaji <nehabalaji10@gmail.com>
2020-10-20 13:39:24 +0530

    add notification worker class

---
+++ app/src/main/java/com/example/quizapp_kotlin/notifications/Notifications.kt
@@ -0,0 +1,4 @@
+package com.example.quizapp_kotlin.notifications
+
+class Notifications {
+}
---
+++ app/src/main/java/com/example/quizapp_kotlin/notifications/NotificationsWorker.kt
@@ -0,0 +1,21 @@
+package com.example.quizapp_kotlin.notifications
+
+import android.app.Application
+import android.content.Context
+import androidx.annotation.NonNull
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.quizapp_kotlin.database.QuizRepository
+
+class NotificationsWorker(@NonNull context: Context, @NonNull workerParameters: WorkerParameters): Worker(context.applicationContext, workerParameters) {
+
+    private val quizRepo: QuizRepository
+    init {
+        quizRepo = QuizRepository.getRepository(context.applicationContext as Application)!!
+    }
+
+    override fun doWork(): Result {
+       var quiz = quizRepo.getRandomState()
+        return Result.success()
+    }
+}
